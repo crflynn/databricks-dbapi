ee231e85f1458b4d42b6e067db7ea589e7b395f2
crflynn <crf204@gmail.com>
2018-12-12 22:13:01 -0500

    azure compatibility

--- databricks_dbapi/databricks.py
+++ databricks_dbapi/databricks.py
@@ -1,4 +1,4 @@
-"""Provide an function to create a Hive connection to a Databricks cluster."""
+"""Provide a function to create a Hive connection to a Databricks cluster."""
 import base64
 import sys
 
@@ -9,7 +9,25 @@ from thrift.transport import THttpClient
 PY_MAJOR = sys.version_info[0]
 
 
-def connect(host, cluster, token=None, user=None, password=None):
+def connect(host, cluster=None, http_path=None, token=None, user=None, password=None):
+    """Create a Hive DBAPI connection to an interactive Databricks cluster.
+
+    Create a DBAPI connection to a Databricks cluster, which can be used to generate
+    DBAPI cursor(s). Provide either a cluster name OR an http_path from the cluster's
+    JDBC/ODBC connection details. If using Azure, http_path is required. On
+    instantiation, http_path is prioritized over cluster.
+
+    For authentication, provide either a token OR both a user and password. Token
+    authentication is strongly preferred.
+
+    :param str host: the server hostname from the cluster's JDBC/ODBC connection page.
+    :param str cluster: the cluster unique name or alias.
+    :param str http_path: the HTTP Path as shown in the cluster's JDBC/ODBC connection
+        page. Required if using Azure platform.
+    :param str token: a Databricks API token.
+    :param str user: a Databricks user name.
+    :param str password: the corresponding Databricks user's password.
+    """
     if token is not None:
         auth = "token:%s" % token
     elif user is not None and password is not None:
@@ -24,7 +42,14 @@ def connect(host, cluster, token=None, user=None, password=None):
     else:
         auth = base64.standard_b64encode(auth.encode()).decode()
 
-    url = "https://%s:443/sql/protocolv1/o/0/%s" % (host, cluster)
+    if http_path is not None:
+        url = "https://%s:443/%s" % (host, http_path)
+    elif cluster is not None:
+        url = "https://%s:443/sql/protocolv1/o/0/%s" % (host, cluster)
+    else:
+        raise ValueError(
+            "Missing arguments. Must provide either cluster or http_path."
+        )
 
     transport = THttpClient.THttpClient(url)
     transport.setCustomHeaders({"Authorization": "Basic %s" % auth})
