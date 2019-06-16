7947afc9b48201c40b2bb8686c20f067a522ecf0
Evan Thomas <evanjt1@gmail.com>
2019-06-16 01:45:22 -0400

    Add DatabricksDialect for SQLAlchemy which inherits from HiveDialect. Implement create_connect_args method with support for 'http_path' option.

---
+++ databricks_dbapi/sqlalchemy_databricks.py
@@ -0,0 +1,23 @@
+from databricks_dbapi import databricks
+from pyhive.sqlalchemy_hive import HiveDialect
+
+
+class DatabricksDialect(HiveDialect):
+    @classmethod
+    def dbapi(cls):
+        return databricks
+
+    def create_connect_args(self, url):
+        kwargs = {
+            'host': url.host,
+            'port': url.port or 443,
+            'database': url.database or 'default',
+            'user': url.username,
+            'password': url.password
+        }
+
+        if url.query is not None and "http_path" in url.query:
+            kwargs["http_path"] = url.query["http_path"]
+
+        kwargs.update(url.query)
+        return [], kwargs
